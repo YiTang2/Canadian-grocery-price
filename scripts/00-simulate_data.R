dd7f4bb32579274e7759df55aa40c38b12138207
YiTang2 <zachary.tang@mail.utoronto.ca>
2024-11-14 09:24:44 -0500

    update
    set the basic structure of the repo

---
+++ scripts/00-simulate_data.R
@@ -0,0 +1,41 @@
+#### Preamble ####
+# Purpose: Simulates a dataset of Australian electoral divisions, including the 
+  #state and party that won each division.
+# Author: Rohan Alexander
+# Date: 26 September 2024
+# Contact: rohan.alexander@utoronto.ca
+# License: MIT
+# Pre-requisites: The `tidyverse` package must be installed
+# Any other information needed? Make sure you are in the `starter_folder` rproj
+
+
+#### Workspace setup ####
+library(tidyverse)
+set.seed(6)
+
+
+#### Simulate data ####
+# Vendor names
+Vendor <- c(
+  "Galleria",
+  "Loblaws",
+  "Metro",
+  "NoFrills",
+  "SaveOnFoods",
+  "TandT",
+  "Voila",
+  "Walmart"
+)
+
+# Create a dataset by randomly assigning states and parties to divisions
+simulated_data <- tibble(
+  Vendor = sample(Vendor, size = 1000, replace = TRUE),  # 1000 size of random vendor names
+  current_price = round(runif(1000, 0, 500), 3),
+  old_price = round(runif(1000, 0, 1000), 3),
+  price_per_unit = current_price / sample(1:10, 1000, replace = TRUE)
+
+)
+
+
+#### Save data ####
+write_csv(simulated_data, "data/00-simulated_data/simulated_data.csv")
---
+++ scripts/01-test_simulated_data.R
@@ -0,0 +1,51 @@
+#### Preamble ####
+# Purpose: Tests the structure and validity of the simulated Australian 
+  #electoral divisions dataset.
+# Author: Rohan Alexander
+# Date: 26 September 2024
+# Contact: rohan.alexander@utoronto.ca
+# License: MIT
+# Pre-requisites: 
+  # - The `tidyverse` package must be installed and loaded
+  # - 00-simulate_data.R must have been run
+# Any other information needed? Make sure you are in the `starter_folder` rproj
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(testthat)
+
+analysis_data <- read_csv("data/00-simulated_data/simulated_data.csv", show_col_types = FALSE)
+
+# Test if the data was successfully loaded
+if (exists("analysis_data")) {
+  message("Test Passed: The dataset was successfully loaded.")
+} else {
+  stop("Test Failed: The dataset could not be loaded.")
+}
+
+
+#### Test data ####
+
+# Test dataset has 1000 rows
+
+test_that("Dataset has 1000 rows", {
+  expect_equal(nrow(simulated_data), 1000)
+})
+
+# Test Data columns are of correct types
+
+test_that("Data columns are of correct types", {
+  expect_is(simulated_data$Vendor, "character")
+  expect_is(simulated_data$current_price, "numeric")
+  expect_is(simulated_data$old_price, "numeric")
+  expect_is(simulated_data$price_per_unit, "numeric")
+})
+
+
+# Test prices are within expected range
+
+test_that("Prices are within expected range", {
+  expect_true(all(simulated_data$current_price >= 0 & simulated_data$current_price <= 500))
+  expect_true(all(simulated_data$old_price >= 0 & simulated_data$old_price <= 1000))
+})
